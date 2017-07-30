01ff67f10e86f0425df7252988d8547d4d2a479e
Joe Darby <joedarby87@gmail.com>
2017-07-30 20:08:03 +0100

    remove junk files

---
+++ AWS_Download.py
@@ -0,0 +1,25 @@
+import boto3
+from pymongo import MongoClient
+
+import AWS_Tools
+import DB_Tools
+
+def main():
+    s3 = boto3.resource('s3')
+    dataBucket = s3.Bucket('jdarby-msc')
+
+    dbClient = MongoClient()
+    db = dbClient.alcosensing
+
+    AWS_Tools.update_users(dataBucket, db)
+    #AWS_Tools.check_files(dataBucket)
+    AWS_Tools.update_files(dataBucket, db)
+    AWS_Tools.update_survey_info(db)
+    AWS_Tools.check_data_complete(db)
+
+    DB_Tools.print_users(db)
+
+
+if __name__ == '__main__':
+    main()
+
